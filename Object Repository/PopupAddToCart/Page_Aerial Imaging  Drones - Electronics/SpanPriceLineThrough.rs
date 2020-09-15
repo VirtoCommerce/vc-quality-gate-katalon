eb8c2784c0e93bb7eb1cba3a98d425f6124d2ba5
AlexTzykin <atz@virtoway.com>
2020-09-15 09:34:55 +0300

    Create new test suite. Add checks when use coupon

--- Object Repository/PopupAddToCart/Page_Aerial Imaging  Drones - Electronics/SpanPriceLineThrough.rs
+++ Object Repository/PopupAddToCart/Page_Aerial Imaging  Drones - Electronics/SpanPriceLineThrough.rs
@@ -1,43 +1,36 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>ButtonAddToCartIcon</name>
+   <name>SpanPriceLineThrough</name>
    <tag></tag>
-   <elementGuidId>e8be523b-cc45-4810-beec-64981941ce5d</elementGuidId>
+   <elementGuidId>19f0d1bc-3a47-4b4d-a10d-58c383f6a07a</elementGuidId>
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//button[@interactor-arg = '${GlobalVariable.productId}' and @title = 'Add to cart']</value>
+         <value>//*[@tab = 'span' and @style = 'text-decoration: line-through;' and @class = 'ng-binding ng-scope']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tab</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>style</name>
+      <type>Main</type>
+      <value>text-decoration: line-through;</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>class</name>
       <type>Main</type>
-      <value>btn btn-secondary AddShopCart ng-scope</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>tag</name>
-      <type>Main</type>
-      <value>button</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>interactor-arg</name>
-      <type>Main</type>
-      <value>${GlobalVariable.productId}</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>title</name>
-      <type>Main</type>
-      <value>Add to cart</value>
+      <value>ng-binding ng-scope</value>
    </webElementProperties>
 </WebElementEntity>
