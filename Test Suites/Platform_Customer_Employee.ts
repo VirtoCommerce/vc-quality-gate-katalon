631ee06c3992123f43691e5428408d9fbf2a0ec2
Volha <baban.volha@gmail.com>
2020-10-14 17:13:08 +0300

    Add test suite for employee test cases

---
+++ Test Suites/Platform_Customer_Employee.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platform_Customer_Employee</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>330aee4f-b4e5-41eb-abcd-3aee3fcc5b6d</testSuiteGuid>
+   <testCaseLink>
+      <guid>1147dbd6-e3e7-4aac-8289-fee16f042021</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API Coverage/Authorization/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a512b4bf-2905-4bad-b433-8a7610ea8286</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API Coverage/Authorization/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96e158b3-af71-487a-a0d4-623e492bce10</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API Coverage/Contacts/OrganizationCreation</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe2975be-bdba-4186-81ec-09b386f4d83a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Contacts/EmployeesCreation</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>30ec59a9-2755-4131-aee1-d6bbc73bf13d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API Coverage/Contacts/ContactCreation</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ed3379c-80c3-446c-ad25-ea48bd2148b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Contacts/EmployeesGet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2c665c7c-e3f3-402b-8ea4-48e897b3fda9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Contacts/EmployeesUpdateBulk</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
