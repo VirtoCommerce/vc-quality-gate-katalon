98deca2774ca15249e4a25ced07779dbb1dd9cc8
Volha <baban.volha@gmail.com>
2021-01-12 18:28:16 +0300

    VP-5668: Add api to get jobs 1 status (#41)
    * Add api to get job /api/platform/jobs/{id}
    * removed the body from GET request
    * added to the request authentification by api_key 
    * added verification to the testcase e.g. using WS.verifyElementPropertyValue() that "job" has worked

---
+++ Test Suites/Platfrom_job.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platfrom_job</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a7f91ae0-ce09-4d51-adff-0f8d9904ce2b</testSuiteGuid>
+   <testCaseLink>
+      <guid>8cbaeb4c-17a8-412a-b139-d3162a163c9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Authorization/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8432e8e-6811-4679-9588-b979e402f3ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Authorization/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a2f94fe4-57ad-426b-9db4-049176c71d43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/BackgroundJobGetStatus</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
