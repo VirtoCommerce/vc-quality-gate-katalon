77a234927c6f6c1db079cb9f4263de7d413ed7fb
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-03-26 11:20:48 +0300

    add check access to file

--- Object Repository/API/backWebServices/VirtoCommerce.Platform/AssetFileAccess.rs
+++ Object Repository/API/backWebServices/VirtoCommerce.Platform/AssetFileAccess.rs
@@ -1,33 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>AssetUploadFile</name>
+   <name>AssetFileAccess</name>
    <tag></tag>
-   <elementGuidId>5db6c83c-48dc-418f-b174-52cc5119c341</elementGuidId>
+   <elementGuidId>6429dd8d-6df0-4c4a-b0f1-902cfe698f44</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <connectionTimeout>-1</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
-   <httpBodyContent>{
-  &quot;contentType&quot;: &quot;multipart/form-data&quot;,
-  &quot;charset&quot;: &quot;UTF-8&quot;,
-  &quot;parameters&quot;: [
-    {
-      &quot;name&quot;: &quot;file1&quot;,
-      &quot;value&quot;: &quot;D:\\Downloads\\123.zip&quot;,
-      &quot;type&quot;: &quot;File&quot;,
-      &quot;contentType&quot;: &quot;application/zip&quot;
-    }
-  ]
-}</httpBodyContent>
-   <httpBodyType>form-data</httpBodyType>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>Content-Type</name>
       <type>Main</type>
-      <value>multipart/form-data</value>
+      <value>application/json</value>
    </httpHeaderProperties>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
@@ -39,8 +28,8 @@
    <katalonVersion>7.9.1</katalonVersion>
    <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
-   <restRequestMethod>POST</restRequestMethod>
-   <restUrl>${GlobalVariable.urlBack}/api/platform/assets?folderUrl=${folderUrl}&amp;url=${url}</restUrl>
+   <restRequestMethod>HEAD</restRequestMethod>
+   <restUrl>${fileUrl}</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
@@ -50,18 +39,11 @@
    <socketTimeout>-1</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
    <variables>
-      <defaultValue>'http://localhost:9104/assets/qwefolder'</defaultValue>
-      <description></description>
-      <id>1c7ff1bb-764e-4657-9382-9e432f7a1237</id>
-      <masked>false</masked>
-      <name>folderUrl</name>
-   </variables>
-   <variables>
       <defaultValue>''</defaultValue>
       <description></description>
-      <id>283376ab-a096-420c-9ad4-e8c70cc047d5</id>
+      <id>cc65020b-d5c4-4b47-b0a9-481ab383941b</id>
       <masked>false</masked>
-      <name>url</name>
+      <name>fileUrl</name>
    </variables>
    <verificationScript>import static org.assertj.core.api.Assertions.*
 
@@ -70,18 +52,8 @@ import com.kms.katalon.core.testobject.ResponseObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webservice.verification.WSResponseManager
 
-import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
-import internal.GlobalVariable as GlobalVariable
-import com.kms.katalon.core.util.KeywordUtil
 
-
-RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
-
-def listJsonOutput = JsonOutput.prettyPrint(response.getResponseBodyContent())
-KeywordUtil.logInfo(listJsonOutput)
-WS.verifyResponseStatusCode(response, 200)
 </verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
