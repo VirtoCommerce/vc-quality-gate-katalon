985f22281a84e1f658ce2f90e8d65a54a1ea181d
Volha <baban.volha@gmail.com>
2021-05-24 18:01:15 +0300

    Add get all setting API

---
+++ Test Suites/Platform_Settings.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platform_Settings</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b02b8556-b0fd-408e-92dd-daaeab2d9819</testSuiteGuid>
+   <testCaseLink>
+      <guid>171f631f-81c7-4602-b0f4-0cc93364f4d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/GetAllSettings</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
