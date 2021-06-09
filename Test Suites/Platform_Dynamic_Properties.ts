eeeea921d5cf7f3263cccae5ceeffe40b74c6f64
Volha <baban.volha@gmail.com>
2021-06-09 12:52:27 +0300

    Add get dynamic properties types

---
+++ Test Suites/Platform_Dynamic_Properties.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platform_Dynamic_Properties</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e7701093-a2ac-4d6c-b087-c6ae0c9628b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>bfb640d3-a72b-45b1-b200-b3e54b20f05d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesGetTypes</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
