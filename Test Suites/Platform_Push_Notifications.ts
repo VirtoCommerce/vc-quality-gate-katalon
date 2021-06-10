4a388d8e830a17c4c411c3e0c10d0434d19ca0e1
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-06-10 18:22:46 +0300

    add apikey for push suite (#101)

--- Test Suites/Platform_Push_Notifications.ts
+++ Test Suites/Platform_Push_Notifications.ts
@@ -12,6 +12,18 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>73cc71ba-4053-4e51-a41d-8ccb74b8c66c</testSuiteGuid>
    <testCaseLink>
+      <guid>d94a3abb-ac9e-4499-bfbf-32963ac12c64</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>16b78f11-daf6-4120-8dd1-47a43273f2b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
       <guid>53289cd3-b7a3-4953-ac76-52570c9e89c9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
