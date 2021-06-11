3f9002dea919ae33196f392a0ecdbf2c799236f5
Volha <baban.volha@gmail.com>
2021-06-11 16:56:25 +0300

    VP-5664: Add setting API to katalon (#96)
    * Add get all setting API
    
    * Update Setting API
    
    * Add API ti get setting by moduleID
    
    * Update name of object and name
    
    * Update API
    
    * Add api to get setting state by name
    
    * SettingUpdated verification in testcase
    
    * Updated comments
    
    * Renaming
    
    * Update markaups in test cases
    
    * Update log with prettifier
    
    * Added description of test cases
    
    * Make last chages, updated requests verification
    
    * Add UI customization request
    
    Co-authored-by: Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>

---
+++ Object Repository/API/backWebServices/VirtoCommerce.Platform/SettingsGetByName.rs
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>SettingsGetByName</name>
+   <tag></tag>
+   <elementGuidId>73ddf32a-295b-4ff3-8750-49292d392718</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>api_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.api_key}</value>
+   </httpHeaderProperties>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/platform/settings/${name}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'VirtoCommerce.ModulesAutoInstallState'</defaultValue>
+      <description></description>
+      <id>b5e8170f-182c-49df-90fc-a7456d8de33a</id>
+      <masked>false</masked>
+      <name>name</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import com.kms.katalon.core.util.KeywordUtil
+import groovy.json.JsonOutput
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+def listJsonOutput = JsonOutput.prettyPrint(response.getResponseBodyContent())
+KeywordUtil.logInfo(listJsonOutput)
+WS.verifyResponseStatusCode(response, 200)
+
+
+
+
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/Platform_Settings.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platform_Settings</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b02b8556-b0fd-408e-92dd-daaeab2d9819</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c8f2d3d-49dc-4a95-bea4-65e9f539d261</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5c83b93a-3c2c-449b-9d0b-6ed870403e84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>171f631f-81c7-4602-b0f4-0cc93364f4d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/SettingsGetAll</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3b3055f-e0cc-40d2-b1c2-cd378c5034be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/SettingsUpdateBoolean</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9eb3a364-7906-44c1-801d-679a07655af7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/SettingsGetByModuleId</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9b0a6f01-9369-4030-a44a-d1797d9772ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/SettingsGetByName</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>305a29a1-2639-41d6-8d27-df4dec5c997a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/SettingsGetUICustomization</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
