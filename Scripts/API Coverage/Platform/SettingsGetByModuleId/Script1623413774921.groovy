a2cc3a4626eab8fd810efc438cb3f7d0173f377f
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-06-18 15:26:48 +0300

    settings fix module Id parameter (#106)
    * update request get module by Id

--- Scripts/API Coverage/Platform/SettingsGetByModuleId/Script1623413774921.groovy
+++ Scripts/API Coverage/Platform/SettingsGetByModuleId/Script1623413774921.groovy
@@ -19,9 +19,9 @@ import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
 WebUI.comment('TEST CASE: Get info about all setting within one module')
 
-response = WS.sendRequestAndVerify(findTestObject('Object Repository/API/backWebServices/VirtoCommerce.Platform/SettingsGetByModuleId', [
-	('moduleId') : 'VirtoCommerce.CustomerSegments'
+response = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.Platform/SettingsGetByModuleId', [
+	('moduleId') : 'VirtoCommerce.Notifications'
 	]))
 
-WS.verifyElementPropertyValue(response, '[0].moduleId', 'VirtoCommerce.CustomerSegments')
-WS.verifyElementPropertyValue(response, '[0].groupName', 'Customer Segments|General')
+WS.verifyElementPropertyValue(response, '[0].moduleId', 'VirtoCommerce.Notifications')
+WS.verifyElementPropertyValue(response, '[0].groupName', 'Notifications|General')
