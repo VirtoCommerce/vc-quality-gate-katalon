d9ddd2c8dfe5525a032ed0558d299510d078e783
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-06-22 11:14:01 +0300

    disable failed cases from collection

--- Test Suites/Modules/Platform_collection.ts
+++ Test Suites/Modules/Platform_collection.ts
@@ -21,7 +21,7 @@
             <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Platform_PushNotifications</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
