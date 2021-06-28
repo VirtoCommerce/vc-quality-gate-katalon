dc39c54091eb866ea25e2010df7ffbcddd4a5864
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-06-28 09:10:08 +0300

    disable oauth case in collection

--- Test Suites/Modules/Platform_collection.ts
+++ Test Suites/Modules/Platform_collection.ts
@@ -30,7 +30,7 @@
             <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Platform_Oauth</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
