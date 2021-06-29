335f81ab8967540818764cf25cf98d2af6f070ca
Volha <baban.volha@gmail.com>
2021-06-29 15:18:20 +0300

    VP-5659: [Auto-tests coverage] Platform module - dynamic/diagnostic (#112)
    * Add get dynamic properties types
    
    * Add get system info
    
    * System info verification errors
    
    * Add get errors
    
    * Add new property
    
    * Add property to store
    
    * Added object of update property
    
    * Put for updating
    
    * Add updating cases
    
    * Fix names of API
    
    * Add delete dynamic property API
    
    * Add property management 
    VirtoCommerce.CustomerModule.Core.Model.Contact
    
    * Add propery management VirtoCommerce.CustomerModule.Core.Model.Employee
    
    * Added description of testcases
    
    * Add property management of VirtoCommerce.CustomerModule.Core.Model.Organization
    
    * Add property management of VirtoCommerce.CustomerModule.Core.Model.Vendor
    
    * Add property management of VirtoCommerce.MarketingModule.Core.Model.DynamicContentItem
    
    * Add property management VirtoCommerce.OrdersModule.Core.Model.CustomerOrder
    
    * Add property management VirtoCommerce.OrdersModule.Core.Model.LineItem
    
    * Add property management of VirtoCommerce.OrdersModule.Core.Model.PaymentIn
    
    * Add property management VirtoCommerce.OrdersModule.Core.Model.Shipment
    
    * Add property manegement VirtoCommerce.QuoteModule.Core.Models.QuoteRequest
    
    * Merge remote-tracking branch 'origin/dev' into VP-5659_dynamic_properties_diagnostic
    
    * Added  AuthBackToken and ApiKeySet to testsuites
    
    * Added dynamicPropertyID global variable
    
    Co-authored-by: Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>

---
+++ Object Repository/API/backWebServices/VirtoCommerce.Platform/DynamicPropertyUpdate.rs
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>DynamicPropertyUpdate</name>
+   <tag></tag>
+   <elementGuidId>4a50e80e-8321-4cf9-9093-cbb7fd847b65</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n   \&quot;name\&quot;:\&quot;${name}\&quot;,\n   \&quot;objectType\&quot;:\&quot;${propertyType}\&quot;,\n   \&quot;valueType\&quot;:\&quot;ShortText\&quot;,\n   \&quot;displayNames\&quot;:[\n      {\n         \&quot;locale\&quot;:\&quot;de-DE\&quot;\n      },\n      {\n         \&quot;locale\&quot;:\&quot;en-US\&quot;\n      },\n      {\n         \&quot;locale\&quot;:\&quot;fr-FR\&quot;\n      }\n   ],\n   \&quot;id\&quot;:\&quot;${GlobalVariable.dynamicPropertyID}\&quot;,\n   \&quot;description\&quot;:\&quot;${description}\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>api_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.api_key}</value>
+   </httpHeaderProperties>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>PUT</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/platform/dynamic/types/${propertyType}/properties/${GlobalVariable.dynamicPropertyID}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'VirtoCommerce.StoreModule.Core.Model.Store'</defaultValue>
+      <description></description>
+      <id>76a79459-5404-45e3-8f34-48b9d02845a2</id>
+      <masked>false</masked>
+      <name>propertyType</name>
+   </variables>
+   <variables>
+      <defaultValue>'UPD new'</defaultValue>
+      <description></description>
+      <id>7d4fc892-d213-4ec8-80fd-ffe13d5adc3f</id>
+      <masked>false</masked>
+      <name>name</name>
+   </variables>
+   <variables>
+      <defaultValue>'description'</defaultValue>
+      <description></description>
+      <id>17d52c5f-55d3-4682-a817-89331c5823d2</id>
+      <masked>false</masked>
+      <name>description</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonOutput
+import com.kms.katalon.core.util.KeywordUtil
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+def prettyJson = JsonOutput.prettyPrint(response.getResponseBodyContent())
+KeywordUtil.logInfo(prettyJson)
+WS.verifyResponseStatusCode(response, 204)
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/Platform_Dynamic_Properties.ts
@@ -0,0 +1,230 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platform_Dynamic_Properties</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e7701093-a2ac-4d6c-b087-c6ae0c9628b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>e973cee5-74df-4882-8041-63c2d0dec902</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96f9dd75-2199-4211-a1a1-c11d5d73cd8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bfb640d3-a72b-45b1-b200-b3e54b20f05d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesGetTypes</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c959411-e7ae-493e-b005-c1062d206089</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.StoreModule.Core.Model.StoreAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db8e8334-3233-47d7-9b98-d731082d2799</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.StoreModule.Core.Model.StoreUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0b898bd4-2f63-437d-8646-cb396ebe011e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.StoreModule.Core.Model.StoreDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c26b2fa9-e05a-4ea3-b146-a9084cf7b022</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.ContactAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf4aea82-1348-4f04-8be6-cd1cad8134f2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.ContactUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bcacb946-766f-4c6b-a679-0fd23805cc89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.ContactDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a73cb2d-7217-41fc-8eb0-6d2cbef9ea0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.EmployeeAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0db19e9-03e7-42cf-b98e-9b7328042f1f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.EmployeeUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0930ff4-7384-4210-beb7-e4b7b582e38e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.EmployeeDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ad1aa4d-f235-4b78-bc47-c4e2310127df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.OrganizationAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da415ebf-b147-45e6-88fd-9e98ff6575bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.OrganizationUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef4a0896-0468-4f81-baa2-9582f773957c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.OrganizationDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>88a1228c-d6cb-4181-9af8-4fd4840b0c04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.VendorAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8eff3351-be86-4487-8e1e-7175bdeb117d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.VendorUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c83cc82-6468-4fbe-8924-a8fee67a4b16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.CustomerModule.Core.Model.VendorDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6a70abd4-e813-42ac-a4f9-d1434f6aacaa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.MarketingModule.Core.Model.DynamicContentItemAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed4e1dba-d166-4566-b51a-9746bc7b377b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.MarketingModule.Core.Model.DynamicContentItemUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>223f0a20-fcd6-446e-bb91-446ae92867af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.MarketingModule.Core.Model.DynamicContentItemDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3d4c6e71-3b0d-482c-83fc-ff8d8779edb1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.CustomerOrderAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3c4a5708-eb51-4e9f-855f-9fbdda6c1c84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.CustomerOrderUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>57e0378f-1249-4b33-81ae-9764c2c55b43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.CustomerOrderDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cca1ca42-b17b-4f07-815c-45f3602b0585</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.LineItemAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8f84898-b99d-4658-af44-d2c62d3096b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.LineItemUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b4fc2a9c-e19c-4d37-99c3-69b10053052b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.LineItemDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa8afd83-0fc1-4357-8a3f-53216e38053e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.PaymentInAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>39ad0f40-5a4e-4e88-8a5e-77f75e5b4a79</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.PaymentInUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a0069876-21bf-494a-aef7-77a5d1110848</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.PaymentInDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ce07da2-e97a-465c-9474-f083d2e954d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.ShipmentAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eb6f850e-ae66-4af5-bfe6-cd20be2de12b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.ShipmentUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7aa7c74-9d67-4047-9f86-fffeba486d8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.OrdersModule.Core.Model.ShipmentDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ed5e38e-1388-4d33-be1f-970f09692d57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.QuoteModule.Core.Models.QuoteRequestAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6514d7ad-8972-4119-807b-c96a619e5355</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.QuoteModule.Core.Models.QuoteRequestUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>70b50475-d897-4274-a7df-8952f3cd3b96</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/Dynamic Properties/DynamicPropertiesVirtoCommerce.QuoteModule.Core.Models.QuoteRequestDelete</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
