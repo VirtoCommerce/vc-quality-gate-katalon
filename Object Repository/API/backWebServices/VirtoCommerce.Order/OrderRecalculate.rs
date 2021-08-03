c194898d7860cdf043dc1869eb11f37f277f7860
Evg652 <60473236+Evg652@users.noreply.github.com>
2021-08-03 18:19:09 +0300

    resolving merge conflict

--- Object Repository/API/backWebServices/VirtoCommerce.Order/OrderRecalculate.rs
+++ Object Repository/API/backWebServices/VirtoCommerce.Order/OrderRecalculate.rs
@@ -33,7 +33,7 @@
    <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>PUT</restRequestMethod>
-   <restUrl>${GlobalVariable.urlBack}/api/order/customerOrders</restUrl>
+   <restUrl>${GlobalVariable.urlBack}/api/order/customerOrders/recalculate</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
