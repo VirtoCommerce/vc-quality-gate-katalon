4a301617eec01b629dc26c7a6955bebfa41ede1d
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-08-03 15:55:05 +0300

    disable dynamic properties in collection

--- Test Suites/Modules/Platform_collection.ts
+++ Test Suites/Modules/Platform_collection.ts
@@ -84,7 +84,7 @@
             <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Platform_Dynamic_Properties</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
