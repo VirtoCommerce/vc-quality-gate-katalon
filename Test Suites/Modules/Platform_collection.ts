59224c5dff8ac52a52ea81b8f035c01f4029d4b0
Evg652 <60473236+Evg652@users.noreply.github.com>
2021-09-21 11:11:42 +0300

    turn content on in the collection (#170)

--- Test Suites/Modules/Platform_collection.ts
+++ Test Suites/Modules/Platform_collection.ts
@@ -102,7 +102,7 @@
             <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Content</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
