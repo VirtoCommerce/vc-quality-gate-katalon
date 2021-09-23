3b1bec070a4c35b223c0393fb4d9a865051bedbf
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-09-23 15:50:48 +0300

    add refactor cases and suite

--- Object Repository/UI/UI-VCFront/main_Call_form_objects/input_Postcode.rs
+++ Object Repository/UI/UI-VCFront/main_Call_form_objects/input_Postcode.rs
@@ -1,34 +1,65 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>textarea_comments</name>
+   <name>input_Postcode</name>
    <tag></tag>
-   <elementGuidId>0f51c79a-771a-43c1-9618-2a49872e8152</elementGuidId>
+   <elementGuidId>b86895da-4803-4e02-8f57-2f34a0b0ba07</elementGuidId>
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//textarea[@id='comments']</value>
-      </entry>
-      <entry>
-         <key>BASIC</key>
-         <value>//textarea[@class = 'form-control']</value>
+         <value>//input[@id='postcode']</value>
       </entry>
       <entry>
          <key>CSS</key>
-         <value>#comments</value>
+         <value>#postcode</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
-      <value>textarea</value>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-segment-name</name>
+      <type>Main</type>
+      <value>postcode</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>postcode</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>postcode</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>placeholder</name>
+      <type>Main</type>
+      <value>Legal postcode</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>text</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>class</name>
       <type>Main</type>
@@ -37,50 +68,29 @@
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
-      <name>id</name>
-      <type>Main</type>
-      <value>comments</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>name</name>
-      <type>Main</type>
-      <value>comments</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>placeholder</name>
-      <type>Main</type>
-      <value>Leave your comments if any</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;comments&quot;)</value>
+      <value>id(&quot;postcode&quot;)</value>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:attributes</name>
       <type>Main</type>
-      <value>//textarea[@id='comments']</value>
+      <value>//input[@id='postcode']</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
       <type>Main</type>
-      <value>//form[@id='contact_us_form']/div[7]/textarea</value>
+      <value>//form[@id='request_license_form']/div[8]/input</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:position</name>
       <type>Main</type>
-      <value>//textarea</value>
+      <value>//div[8]/input</value>
    </webElementXpaths>
 </WebElementEntity>
--- Test Suites/VC-Forms.ts
+++ Test Suites/VC-Forms.ts
@@ -20,61 +20,85 @@
    <testCaseLink>
       <guid>4565c102-432d-4ab4-a93a-54f2b3ab01ee</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/Home</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Page_Home</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>5e93eb8d-d07f-43ab-9da7-0d1a6d0d29db</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/ContactUs</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Page_ContactUs</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>14747240-4caa-47a4-b763-82959d768179</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/RequestDemo</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>96f2991b-5b9b-4d46-804a-d479e6b67549</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/Product/B2BeCommercePlatform</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>79b97392-bce9-40c3-b14e-8c2ce96067ed</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/WhiteLabel_eCommerce</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Page_Request_Demo</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>56871d63-35f8-4ca5-ae45-d5922bd5f0db</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/UI-VCFront/Case_studies_Bulk</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>d6560bed-e4e6-4e8d-abe0-b4eff406fc0b</guid>
+      <guid>4ab5fb89-8e90-4ed4-b1dc-fe5863ed6dbb</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/Whitepapers_Bulk</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Product_Single-PlatformTechnology-Cloud</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0df51458-ba61-437c-9b14-ba21a57de1c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Product_Bulk-Integrations</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01609e75-76f3-43db-845d-8f6b91592fd7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Product_Bulk-PlatformOverview</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>e577ee8e-a93f-46ae-98f4-c242348584be</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/UI-VCFront/Solutions/Solutions_Bulk</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Solutions_Bulk</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>cc960bdf-4de9-4878-8b7d-4d3c5740d75d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/UI-VCFront/Solutions/Solutions_partner_Bulk</testCaseId>
+      <testCaseId>Test Cases/UI-VCFront/Solutions_Bulk-Partners</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a8450070-951c-4383-a64a-35820402c06a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Solutions_Single-Partners-License</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8c7d0806-c015-433b-a148-a45c40b6e37d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Solutions_Single-Partners-Cloud</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d84b8b05-ff7b-47ce-a958-c38a61f404b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/Solutions_Single-Partners-Find</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d77d479-6263-418a-82aa-68794e5416fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UI-VCFront/WhitePapers_Bulk</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>17ee5bfc-47cf-4f5a-b916-70dac2f32ec6</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/SystemCases/CloseBrowser</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
