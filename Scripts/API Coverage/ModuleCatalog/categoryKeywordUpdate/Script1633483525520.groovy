086aa601cedec117a33d78d9cb1a92b0d6940430
Evg652 <60473236+Evg652@users.noreply.github.com>
2021-10-06 04:49:08 +0300

    change tc description for variable changes

--- Scripts/API Coverage/ModuleCatalog/categoryKeywordUpdate/Script1633483525520.groovy
+++ Scripts/API Coverage/ModuleCatalog/categoryKeywordUpdate/Script1633483525520.groovy
@@ -17,4 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+
+'UPDATE KEYWORD'
 GlobalVariable.keyword = GlobalVariable.categoryName
