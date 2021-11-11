f4fb1529cab63b4ead0b12c61009145e114a8815
Evg652 <60473236+Evg652@users.noreply.github.com>
2021-11-11 21:33:55 +0300

    add objects

---
+++ Object Repository/API/backWebServices/VirtoCommerce.Pricing/PricesUpdateBulk.rs
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>PricesUpdateBulk</name>
+   <tag></tag>
+   <elementGuidId>db97a09c-045d-4e03-9004-b2bd565e56c1</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;storeId\&quot;: \&quot;string\&quot;,\n  \&quot;catalogId\&quot;: \&quot;string\&quot;,\n  \&quot;productIds\&quot;: [\n    \&quot;string\&quot;\n  ],\n  \&quot;pricelistIds\&quot;: [\n    \&quot;string\&quot;\n  ],\n  \&quot;returnAllMatchedPrices\&quot;: true,\n  \&quot;quantity\&quot;: 0,\n  \&quot;customerId\&quot;: \&quot;string\&quot;,\n  \&quot;organizationId\&quot;: \&quot;string\&quot;,\n  \&quot;certainDate\&quot;: \&quot;2021-11-11T16:21:30.065Z\&quot;,\n  \&quot;currency\&quot;: \&quot;string\&quot;,\n  \&quot;contextObject\&quot;: {},\n  \&quot;geoCity\&quot;: \&quot;string\&quot;,\n  \&quot;geoState\&quot;: \&quot;string\&quot;,\n  \&quot;geoCountry\&quot;: \&quot;string\&quot;,\n  \&quot;geoContinent\&quot;: \&quot;string\&quot;,\n  \&quot;geoZipCode\&quot;: \&quot;string\&quot;,\n  \&quot;geoConnectionType\&quot;: \&quot;string\&quot;,\n  \&quot;geoTimeZone\&quot;: \&quot;string\&quot;,\n  \&quot;geoIpRoutingType\&quot;: \&quot;string\&quot;,\n  \&quot;geoIspSecondLevel\&quot;: \&quot;string\&quot;,\n  \&quot;geoIspTopLevel\&quot;: \&quot;string\&quot;,\n  \&quot;shopperAge\&quot;: 0,\n  \&quot;shopperGender\&quot;: \&quot;string\&quot;,\n  \&quot;language\&quot;: \&quot;string\&quot;,\n  \&quot;userGroups\&quot;: [\n    \&quot;string\&quot;\n  ],\n  \&quot;shopperSearchedPhraseInStore\&quot;: \&quot;string\&quot;,\n  \&quot;shopperSearchedPhraseOnInternet\&quot;: \&quot;string\&quot;,\n  \&quot;currentUrl\&quot;: \&quot;string\&quot;,\n  \&quot;referredUrl\&quot;: \&quot;string\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>api_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.api_key}</value>
+   </httpHeaderProperties>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>PUT</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/products/prices</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonOutput
+import com.kms.katalon.core.util.KeywordUtil
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+def prettyJson = JsonOutput.prettyPrint(response.getResponseBodyContent())
+KeywordUtil.logInfo(prettyJson)
+WS.verifyResponseStatusCode(response, 200)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
