e016e2434ed47516e6015a77d689d66b7f8d84fd
Evg652 <60473236+Evg652@users.noreply.github.com>
2021-12-09 09:03:51 +0300

    small fix(change testCases order)

--- Test Suites/Modules/Platform_API_ChangeLog.ts
+++ Test Suites/Modules/Platform_API_ChangeLog.ts
@@ -12,16 +12,16 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ab287740-0334-4308-b8d5-4cf6c1d3c31e</testSuiteGuid>
    <testCaseLink>
-      <guid>da3dbce8-4ae7-42dd-b2d8-c1638ed68186</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>1b6b5b7b-b82c-41d2-a292-c741e10de996</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da3dbce8-4ae7-42dd-b2d8-c1638ed68186</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>aa62fd29-0939-4aef-9c33-f36fc9417922</guid>
