5e899cd5ffb90b5ddc83481a1fb2429dc60aabbd
Evg652 <60473236+Evg652@users.noreply.github.com>
2021-12-16 15:20:59 +0300

    VP-7821: Search index (#218)
    * add test cases abd objects
    
    * add module_Search suite

---
+++ Object Repository/API/backWebServices/VirtoCommerce.Search/indexCancel.rs
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>indexCancel</name>
+   <tag></tag>
+   <elementGuidId>a66d1696-cacb-4548-af5f-7e62286b7411</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>api_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.api_key}</value>
+   </httpHeaderProperties>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/search/indexes/tasks/${taskId}/cancel</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonOutput
+import com.kms.katalon.core.util.KeywordUtil
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+def prettyJson = JsonOutput.prettyPrint(response.getResponseBodyContent())
+KeywordUtil.logInfo(prettyJson)
+WS.verifyResponseStatusCode(response, 200)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Scripts/API Coverage/ModuleSearch/indexationRunAndVerify/Script1639644130283.groovy
@@ -0,0 +1,59 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+
+WebUI.comment('TEST CASE: RUN AND VERIFY PLATFORM INDEXATION')
+
+
+'GET THE INITIAL LAST INDEXATION DATE'
+initialIndexation = WS.callTestCase(findTestCase('Test Cases/API Coverage/ModuleSearch/GetIndex'),
+	null)
+documentType = WS.getElementPropertyValue(initialIndexation, '[0].documentType')
+initialDate = WS.getElementPropertyValue(initialIndexation, '[0].lastIndexationDate')
+
+
+'GET THE INITIAL INDEXATION DATE FOR THE PRODUCT'
+productInitialIndexationGet = WS.callTestCase(findTestCase('Test Cases/API Coverage/ModuleSearch/GetIndexForProduct'),
+	null)
+productInitalDate = WS.getElementPropertyValue(productInitialIndexationGet, '[0].indexationdate')
+
+
+'SEND REQUEST TO RUN PLATFORM INDEXATION'
+indexationRun = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.Search/index'))
+WebUI.comment('Waiting for drop index 10 sec')
+WS.delay(10)
+GlobalVariable.taskId = WS.getElementPropertyValue(indexationRun, 'id')//used in index cancel test case
+
+
+'GET THE UPDATED INDEXATION DATE'
+updatedIndexation = WS.callTestCase(findTestCase('Test Cases/API Coverage/ModuleSearch/GetIndex'),
+	null)
+updatedDocumentType = WS.getElementPropertyValue(updatedIndexation, '[0].documentType')
+updatedDate = WS.getElementPropertyValue(updatedIndexation, '[0].lastIndexationDate')
+
+
+'GET THE UPDATED INDEXATION DATE FOR THE PRODUCT'
+productUpdatedIndexationGet = WS.callTestCase(findTestCase('Test Cases/API Coverage/ModuleSearch/GetIndexForProduct'),
+	null)
+productUpdatedDate = WS.getElementPropertyValue(productUpdatedIndexationGet, '[0].indexationdate')
+
+
+'VERIFY THE DATA WAS UPDATED'
+assert documentType == updatedDocumentType
+assert initialDate < updatedDate
+assert productInitalDate < productUpdatedDate
+
