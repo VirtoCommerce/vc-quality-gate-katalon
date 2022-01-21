7377ddbde4709935adb94423ebfff722b511f88f
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-01-21 14:41:32 +0300

    refactor member Create and Update (#233)

--- Scripts/API Coverage/Contacts/_module_Customer_Member/MemberUpdate/Script1634663654679.groovy
+++ Scripts/API Coverage/Contacts/_module_Customer_Member/MemberUpdate/Script1634663654679.groovy
@@ -24,19 +24,66 @@ WebUI.comment("TEST CASE: Member update")
 'MEMBER DATA STORED IN THE LISTS'
 List <String> memberType = GlobalVariable.memberType
 List <String> memberId = GlobalVariable.memberId
+List <String> addressTypes = GlobalVariable.addressTypes
 
 
-'SEND REQUEST TO UPDATE MEMBERS FROM LISTS'
-/*Here TEMP is used to prevent firstName of being overwritten, as
-it's used in delete case in its initial state*/
-GlobalVariable.updatedMemberName  = GlobalVariable.firstName + ' UPD'
-for (int i; i < memberId.size(); i++) {
-    WebUI.comment("MEMBER ID IS : " + memberId.get(i))
-    WebUI.comment("MEMBER TYPE IS : " + memberType.get(i))	
-    updateContact = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.Customer/Members/MemberUpdate', [
+'CREATE ADDRESSES TEMPLATE'
+List <Object>addressesList = []
+for (int b = 0; b < addressTypes.size(); b++) {
+	addressDataTemplate = new File('Data Files/addressDataTemplate.json').text
+	addressDataTemplateDefault = new JsonSlurper().parseText(addressDataTemplate)
+	addressDataTemplateDefault.isDefault = 'true'
+	addressDataTemplateDefault.addressType = addressTypes.get(b)
+	
+	addressDataTemplate = new JsonSlurper().parseText(addressDataTemplate)
+	addressDataTemplate.isDefault = 'false'
+	addressDataTemplate.addressType = addressTypes.get(b)
+	
+	addressDataDefault = new groovy.json.JsonBuilder(addressDataTemplateDefault)
+	addressData = new groovy.json.JsonBuilder(addressDataTemplate)
+	println addressDataDefault
+	println addressData
+
+	addressesList.add(addressDataDefault)
+	addressesList.add(addressData)
+		}
+		addressesString = addressesList.toString()
+		String regex = "\\[|\\]";
+		addresses = addressesString.replaceAll(regex, "");
+		System.out.println(addresses);
+		
+
+'UPDATE CREATED ACCOUNTS'
+for (int c; c < memberId.size(); c++) {
+	WebUI.comment("MEMBER ID IS : " + memberId.get(c))
+	WebUI.comment("MEMBER TYPE IS : " + memberType.get(c))
+	GlobalVariable.updatedMemberName  = GlobalVariable.firstName + 'UPD'
+updateContact =  WS.sendRequestAndVerify(findTestObject('Object Repository/API/backWebServices/VirtoCommerce.Customer/Members/MemberUpdate', [
+		('id') : memberId.get(c),
+		('memberType') : memberType.get(c),
 		('name') : GlobalVariable.updatedMemberName,
-		('id') : memberId.get(i), 
-		('memberType') : memberType.get(i)
+		('addresses')  : addresses
 		]))
 }
 
+
+'VERIFY ACCOUNTS ARE  UPDATED'
+for (int d; d < addressTypes.size(); d++) {
+	for(int e; e < memberId.size(); e++) {
+	membersGetAll = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.Customer/Members/MemberGetId', [
+		('id') : memberId.get(e)
+		]))
+	memberData = new JsonSlurper().parseText(membersGetAll.getResponseBodyContent())
+	List addressesGet = memberData.addresses
+	def x
+	for (int f; f < addressesGet.size(); f++) {
+	if (addressesGet[f].addressType == addressTypes[d] && addressesGet[f].isDefault == true) {
+		println 'match ' + addressTypes[d] + ' ' + addressesGet[f].isDefault
+		x = addressesGet[f].isDefault
+		break
+		} else {
+			println 'no match ' + addressTypes[d] + ' ' + addressesGet[f].isDefault}
+			}
+	assert x == true
+	}
+}
