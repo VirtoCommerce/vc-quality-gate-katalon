a3ebdf971f6681acc2318362d4d1c2f082d715e1
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2022-02-08 16:45:39 +0300

    add cart module objects

---
+++ Object Repository/API/backWebServices/VirtoCommerce.Cart/_DRAFT/CartItemsAdd.rs
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>CartItemsAdd</name>
+   <tag></tag>
+   <elementGuidId>829d7cc7-83d6-422c-b636-b40dc2acbab1</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;productId\&quot;: \&quot;string\&quot;,\n  \&quot;catalogId\&quot;: \&quot;string\&quot;,\n  \&quot;categoryId\&quot;: \&quot;string\&quot;,\n  \&quot;sku\&quot;: \&quot;string\&quot;,\n  \&quot;productType\&quot;: \&quot;string\&quot;,\n  \&quot;name\&quot;: \&quot;string\&quot;,\n  \&quot;quantity\&quot;: 0,\n  \&quot;fulfillmentCenterId\&quot;: \&quot;string\&quot;,\n  \&quot;fulfillmentCenterName\&quot;: \&quot;string\&quot;,\n  \&quot;fulfillmentLocationCode\&quot;: \&quot;string\&quot;,\n  \&quot;shipmentMethodCode\&quot;: \&quot;string\&quot;,\n  \&quot;requiredShipping\&quot;: true,\n  \&quot;thumbnailImageUrl\&quot;: \&quot;string\&quot;,\n  \&quot;imageUrl\&quot;: \&quot;string\&quot;,\n  \&quot;isGift\&quot;: true,\n  \&quot;isRejected\&quot;: true,\n  \&quot;currency\&quot;: \&quot;string\&quot;,\n  \&quot;languageCode\&quot;: \&quot;string\&quot;,\n  \&quot;note\&quot;: \&quot;string\&quot;,\n  \&quot;isReccuring\&quot;: true,\n  \&quot;taxIncluded\&quot;: true,\n  \&quot;volumetricWeight\&quot;: 0,\n  \&quot;weightUnit\&quot;: \&quot;string\&quot;,\n  \&quot;weight\&quot;: 0,\n  \&quot;measureUnit\&quot;: \&quot;string\&quot;,\n  \&quot;height\&quot;: 0,\n  \&quot;length\&quot;: 0,\n  \&quot;width\&quot;: 0,\n  \&quot;validationType\&quot;: \&quot;string\&quot;,\n  \&quot;isReadOnly\&quot;: true,\n  \&quot;priceId\&quot;: \&quot;string\&quot;,\n  \&quot;price\&quot;: {\n    \&quot;pricelistId\&quot;: \&quot;string\&quot;,\n    \&quot;pricelist\&quot;: {\n      \&quot;name\&quot;: \&quot;string\&quot;,\n      \&quot;description\&quot;: \&quot;string\&quot;,\n      \&quot;currency\&quot;: \&quot;string\&quot;,\n      \&quot;outerId\&quot;: \&quot;string\&quot;,\n      \&quot;prices\&quot;: [\n        null\n      ],\n      \&quot;assignments\&quot;: [\n        {\n          \&quot;catalogId\&quot;: \&quot;string\&quot;,\n          \&quot;storeId\&quot;: \&quot;string\&quot;,\n          \&quot;pricelistId\&quot;: \&quot;string\&quot;,\n          \&quot;name\&quot;: \&quot;string\&quot;,\n          \&quot;description\&quot;: \&quot;string\&quot;,\n          \&quot;priority\&quot;: 0,\n          \&quot;startDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n          \&quot;endDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n          \&quot;dynamicExpression\&quot;: {\n            \&quot;all\&quot;: true,\n            \&quot;not\&quot;: true,\n            \&quot;availableChildren\&quot;: [\n              {}\n            ],\n            \&quot;children\&quot;: [\n              {}\n            ]\n          },\n          \&quot;outerId\&quot;: \&quot;string\&quot;,\n          \&quot;createdDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n          \&quot;modifiedDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n          \&quot;createdBy\&quot;: \&quot;string\&quot;,\n          \&quot;modifiedBy\&quot;: \&quot;string\&quot;,\n          \&quot;id\&quot;: \&quot;string\&quot;\n        }\n      ],\n      \&quot;createdDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n      \&quot;modifiedDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n      \&quot;createdBy\&quot;: \&quot;string\&quot;,\n      \&quot;modifiedBy\&quot;: \&quot;string\&quot;,\n      \&quot;id\&quot;: \&quot;string\&quot;\n    },\n    \&quot;currency\&quot;: \&quot;string\&quot;,\n    \&quot;productId\&quot;: \&quot;string\&quot;,\n    \&quot;sale\&quot;: 0,\n    \&quot;list\&quot;: 0,\n    \&quot;minQuantity\&quot;: 0,\n    \&quot;startDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n    \&quot;endDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n    \&quot;outerId\&quot;: \&quot;string\&quot;,\n    \&quot;createdDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n    \&quot;modifiedDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n    \&quot;createdBy\&quot;: \&quot;string\&quot;,\n    \&quot;modifiedBy\&quot;: \&quot;string\&quot;,\n    \&quot;id\&quot;: \&quot;string\&quot;\n  },\n  \&quot;listPrice\&quot;: 0,\n  \&quot;listPriceWithTax\&quot;: 0,\n  \&quot;salePrice\&quot;: 0,\n  \&quot;salePriceWithTax\&quot;: 0,\n  \&quot;placedPrice\&quot;: 0,\n  \&quot;placedPriceWithTax\&quot;: 0,\n  \&quot;extendedPrice\&quot;: 0,\n  \&quot;extendedPriceWithTax\&quot;: 0,\n  \&quot;discountAmount\&quot;: 0,\n  \&quot;discountAmountWithTax\&quot;: 0,\n  \&quot;discountTotal\&quot;: 0,\n  \&quot;discountTotalWithTax\&quot;: 0,\n  \&quot;fee\&quot;: 0,\n  \&quot;feeWithTax\&quot;: 0,\n  \&quot;discounts\&quot;: [\n    {\n      \&quot;promotionId\&quot;: \&quot;string\&quot;,\n      \&quot;currency\&quot;: \&quot;string\&quot;,\n      \&quot;discountAmount\&quot;: 0,\n      \&quot;discountAmountWithTax\&quot;: 0,\n      \&quot;coupon\&quot;: \&quot;string\&quot;,\n      \&quot;description\&quot;: \&quot;string\&quot;,\n      \&quot;id\&quot;: \&quot;string\&quot;\n    }\n  ],\n  \&quot;taxType\&quot;: \&quot;string\&quot;,\n  \&quot;taxTotal\&quot;: 0,\n  \&quot;taxPercentRate\&quot;: 0,\n  \&quot;taxDetails\&quot;: [\n    {\n      \&quot;rate\&quot;: 0,\n      \&quot;amount\&quot;: 0,\n      \&quot;name\&quot;: \&quot;string\&quot;\n    }\n  ],\n  \&quot;dynamicProperties\&quot;: [\n    {\n      \&quot;objectId\&quot;: \&quot;string\&quot;,\n      \&quot;values\&quot;: [\n        {\n          \&quot;objectType\&quot;: \&quot;string\&quot;,\n          \&quot;objectId\&quot;: \&quot;string\&quot;,\n          \&quot;locale\&quot;: \&quot;string\&quot;,\n          \&quot;value\&quot;: {},\n          \&quot;valueId\&quot;: \&quot;string\&quot;,\n          \&quot;valueType\&quot;: \&quot;Undefined\&quot;,\n          \&quot;propertyId\&quot;: \&quot;string\&quot;,\n          \&quot;propertyName\&quot;: \&quot;string\&quot;\n        }\n      ],\n      \&quot;name\&quot;: \&quot;string\&quot;,\n      \&quot;description\&quot;: \&quot;string\&quot;,\n      \&quot;objectType\&quot;: \&quot;string\&quot;,\n      \&quot;isArray\&quot;: true,\n      \&quot;isDictionary\&quot;: true,\n      \&quot;isMultilingual\&quot;: true,\n      \&quot;isRequired\&quot;: true,\n      \&quot;displayOrder\&quot;: 0,\n      \&quot;valueType\&quot;: \&quot;Undefined\&quot;,\n      \&quot;displayNames\&quot;: [\n        {\n          \&quot;locale\&quot;: \&quot;string\&quot;,\n          \&quot;name\&quot;: \&quot;string\&quot;\n        }\n      ],\n      \&quot;createdDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n      \&quot;modifiedDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n      \&quot;createdBy\&quot;: \&quot;string\&quot;,\n      \&quot;modifiedBy\&quot;: \&quot;string\&quot;,\n      \&quot;id\&quot;: \&quot;string\&quot;\n    }\n  ],\n  \&quot;createdDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n  \&quot;modifiedDate\&quot;: \&quot;2022-02-07T20:13:01.873Z\&quot;,\n  \&quot;createdBy\&quot;: \&quot;string\&quot;,\n  \&quot;modifiedBy\&quot;: \&quot;string\&quot;,\n  \&quot;id\&quot;: \&quot;string\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>api_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.api_key}</value>
+   </httpHeaderProperties>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/carts/${cartId}/items</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>03f7967d-31e9-44aa-8200-4b4153f06a27</id>
+      <masked>false</masked>
+      <name>cartId</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonOutput
+import com.kms.katalon.core.util.KeywordUtil
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+def prettyJson = JsonOutput.prettyPrint(response.getResponseBodyContent())
+KeywordUtil.logInfo(prettyJson)
+WS.verifyResponseStatusCode(response, 204)
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Object Repository/API/backWebServices/VirtoCommerce.Cart/_DRAFT/CartsGetFiltered.rs
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>CartsGetFiltered</name>
+   <tag></tag>
+   <elementGuidId>e3b4c175-116e-41c6-9f3d-be8fc421a706</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>text/plain</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>api_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.api_key}</value>
+   </httpHeaderProperties>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/carts/${storeId}/${customerId}/${cartName}/${currency}/${cultureName}/current</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description>id of the existing Store</description>
+      <id>e01223e6-8437-4a3d-8e6f-d8198d9ef6d8</id>
+      <masked>false</masked>
+      <name>storeId</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>a537c91b-a166-4a46-bb69-fb5747136e78</id>
+      <masked>false</masked>
+      <name>customerId</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>c269106b-df9f-4c2c-8168-d80a4a2fd9ee</id>
+      <masked>false</masked>
+      <name>cartName</name>
+   </variables>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>151e48ff-3d3c-406c-ae09-06a3ad4a0911</id>
+      <masked>false</masked>
+      <name>cultureName</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonOutput
+import com.kms.katalon.core.util.KeywordUtil
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+def prettyJson = JsonOutput.prettyPrint(response.getResponseBodyContent())
+KeywordUtil.logInfo(prettyJson)
+WS.verifyResponseStatusCode(response, 200)
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
