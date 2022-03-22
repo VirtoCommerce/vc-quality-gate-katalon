e7584a6f7a72a99569797f848d766213216212eb
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-03-22 18:14:14 +0300

    turn apikey cases on

--- Test Suites/DRAFT/Debug.ts
+++ Test Suites/DRAFT/Debug.ts
@@ -14,13 +14,13 @@
    <testCaseLink>
       <guid>776516d4-10e6-488a-9113-aeba8fe6e62e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>47873c37-9bf5-40e4-bc4b-a6be95427d4d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
    </testCaseLink>
    <testCaseLink>
