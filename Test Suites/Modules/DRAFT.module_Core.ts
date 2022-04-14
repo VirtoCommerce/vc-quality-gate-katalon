9b94080bfaadf2a3723143d9b25994acc6fcf4e1
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-04-14 14:14:16 +0300

    core_debug

--- Test Suites/Modules/DRAFT.module_Core.ts
+++ Test Suites/Modules/DRAFT.module_Core.ts
@@ -11,4 +11,40 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>704effa9-48ab-4d66-bfe7-ace0a08f5f71</testSuiteGuid>
+   <testCaseLink>
+      <guid>01e9746a-cc7b-4d6c-9d62-d102f4bbff30</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2a16c566-581a-4389-9996-1b748dc171ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e5248f08-0ffa-4776-8576-12ad2c5cc02e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleCore/coreUnitSettingsCreateUpdateDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ae16380f-8207-4dc3-9221-f6726fc5ad54</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleCore/currenciesCreateUpdateDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>97c40c1d-59d2-423d-90d2-aefebe25769b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleCore/packageTypeCreateUpdateDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bff36fd5-867b-4219-b481-8bdc9868c6d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleCore/seoInfoGetRequestsCheck</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
