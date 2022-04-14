a4e9235c4c52da3b2624b2f3512733a744a07777
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-04-14 14:13:35 +0300

    core_debug

--- Test Suites/Modules/DRAFT.module_Core.ts
+++ Test Suites/Modules/DRAFT.module_Core.ts
@@ -10,41 +10,5 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>e685a78c-2bbd-4121-bd8d-df55802fcafb</testSuiteGuid>
-   <testCaseLink>
-      <guid>08a9ebce-8efd-4a98-9aef-c567a17d626c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a28c7619-c150-45ae-88f2-8bbb34b21090</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>5b5a6ad5-0629-4b09-afb0-f4968761a487</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/ModuleCore/coreUnitSettingsCreateUpdateDelete</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>29824f4b-a91f-409b-b120-bcef7a2af7a6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/ModuleCore/currenciesCreateUpdateDelete</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>09248c7d-d5a7-4b4b-a50a-218e01443412</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/ModuleCore/packageTypeCreateUpdateDelete</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>16f6e925-3f47-43f1-9d34-5e844161cf04</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API Coverage/ModuleCore/seoInfoGetRequestsCheck</testCaseId>
-   </testCaseLink>
+   <testSuiteGuid>704effa9-48ab-4d66-bfe7-ace0a08f5f71</testSuiteGuid>
 </TestSuiteEntity>
