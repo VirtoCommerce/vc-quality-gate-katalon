80c0db63eacf774f502f0ecfda2baa0dc796d912
userfix <nosarevk.@gmail.com>
2022-04-18 21:28:42 +0300

    debug asset creation

--- Test Suites/DRAFT/Platform.Assets.ts
+++ Test Suites/DRAFT/Platform.Assets.ts
@@ -26,19 +26,19 @@
    <testCaseLink>
       <guid>cd9c6251-ad9a-4613-9fff-69525d7a9c1a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFolderDelete2</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c4b794e2-3625-465e-ba89-12dd9fe3d1da</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFolderCreateErrorValidation</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>47ea097c-c48f-4f7b-b973-93d0160ff0c1</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFolderGet</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -56,67 +56,67 @@
    <testCaseLink>
       <guid>60f60fe8-86b8-4a0b-9def-57a323c6cd8c</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFolderGet</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9418346c-f79b-4517-8266-5c763773d0a5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileUploadUrlImage</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>f3099dc2-6e3d-4695-8e84-cb16a59f6a18</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileAccess</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>da6fde6a-c3e8-4be1-b28f-613c5661add5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileUploadUrl</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a8b5cc45-b7d2-455c-80cc-d74d8697000d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileAccess</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>aaabd52f-bdab-4309-bbdb-4fc98f21afd0</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileUploadLocal</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>606c693a-bd85-4f0d-9e6d-1d41e78ab466</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileAccess</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>22959507-029f-4e19-b498-88d37c20942c</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileUploadLocalStorage</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>426db9e9-d0f3-47a9-85da-de156b29971a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFolderDelete</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>74dfa3d2-8f6d-480e-bdb7-f382938a520d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFileAccessAfterDelete</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>8e15de69-d13f-4de2-bc40-87b7c15be0b6</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Asset/AssetFolderCreateDeleteBulk</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
