023143aa510ca7017c67ed09d27bf67c13024cc6
userfix <nosarevk.@gmail.com>
2022-04-20 15:27:11 +0300

    add suites

--- Test Suites/DRAFT/DeHavilland.ts
+++ Test Suites/DRAFT/DeHavilland.ts
@@ -114,7 +114,7 @@
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Catalog_Products</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
--- Test Suites/DeHavilland.ts
+++ Test Suites/DeHavilland.ts
@@ -64,7 +64,7 @@
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Platform_ChangeLog</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
