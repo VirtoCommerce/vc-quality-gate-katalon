3e401ef83b9d4ea4a8cc6126c1566e03c70b5fe8
userfix <nosarevk.@gmail.com>
2022-04-20 16:24:30 +0300

    add debug collection

--- Test Suites/DRAFT/DeHavilland-dbg.ts
+++ Test Suites/DRAFT/DeHavilland-dbg.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>DeHavilland</name>
+   <name>DeHavilland-dbg</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
