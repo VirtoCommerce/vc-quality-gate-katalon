daddcb31f71846ece70dce985917ec4018729fb9
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-06-28 00:49:18 +0300

    fix the added test case guid in the suit, so it would be available in the github  containet

--- Test Suites/DRAFT/Platfrom_modules.ts
+++ Test Suites/DRAFT/Platfrom_modules.ts
@@ -24,9 +24,9 @@
       <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>fb5e75a7-4be7-4647-b0c7-171f4fc368ad</guid>
+      <guid>76a40d19-95dc-4b4b-bc66-1fee8e79d116</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Modules/modulesManagement</testCaseId>
-   </testCaseLink>
+       </testCaseLink>
 </TestSuiteEntity>
