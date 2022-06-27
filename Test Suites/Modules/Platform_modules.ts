1aa2944b09f6c7a972328d92ea616df161c0c00a
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-06-28 01:22:00 +0300

    debug

--- Test Suites/Modules/Platform_modules.ts
+++ Test Suites/Modules/Platform_modules.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Platfrom_modules</name>
+   <name>Platform_modules</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
-   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeout>10</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>ee00357b-b4d3-4604-b85a-7e07d8962879</testSuiteGuid>
+   <testSuiteGuid>23485d42-a6f3-4750-b1b5-dd387261af72</testSuiteGuid>
    <testCaseLink>
-      <guid>d4318587-ecc0-4a65-9481-5365393670ad</guid>
+      <guid>e8e2be76-ad41-498b-8af6-093c21402311</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>4d5f00be-8bf1-4378-bea2-ee5cb3c3f24f</guid>
+      <guid>b032ec2a-c6b7-4b1d-ab99-6b044f447b9e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>76a40d19-95dc-4b4b-bc66-1fee8e79d116</guid>
+      <guid>f4955b35-2e46-4e82-a58a-88af0dbaf31b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/Modules/modulesManagement</testCaseId>
-       </testCaseLink>
+   </testCaseLink>
 </TestSuiteEntity>
