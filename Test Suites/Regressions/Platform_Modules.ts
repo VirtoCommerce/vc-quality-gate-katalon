516261ae482957be27e96abf762313ae2ac6b8c9
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-06-29 16:00:26 +0300

    add modules suit to regressions folder

---
+++ Test Suites/Regressions/Platform_Modules.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Platform_Modules</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0f92271d-0551-4dd3-b492-e80c5ff08f4f</testSuiteGuid>
+   <testCaseLink>
+      <guid>e8e2be76-ad41-498b-8af6-093c21402311</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b032ec2a-c6b7-4b1d-ab99-6b044f447b9e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>59fa339a-1ae1-4ed4-8cde-aba742dfc531</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/Platform/ModulesManagement/modulesManagement</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
