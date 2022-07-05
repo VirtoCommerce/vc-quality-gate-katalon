02284102fa4393048bb0e493f365366f2a7267b2
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-07-05 08:45:25 +0300

    consolidate all marketing related suites into one

--- Test Suites/Modules/module_Marketing.ts
+++ Test Suites/Modules/module_Marketing.ts
@@ -10,39 +10,129 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>458007f3-e973-47c1-8c79-6d2841a4cf66</testSuiteGuid>
+   <testSuiteGuid>9ae0adf0-a7be-4c7c-afb7-62145250ac8b</testSuiteGuid>
    <testCaseLink>
-      <guid>8bd1cbe9-9315-4955-a97c-7763c4a7d3ee</guid>
+      <guid>d5ebd59f-8f44-4f81-8f02-c38d0fceef2e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>ef56195c-8764-424e-8132-7aa4109cc3f8</guid>
+      <guid>710c60f2-0035-49df-b492-d7f9d569d395</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>722c486b-2300-4286-abc0-0d902d60b166</guid>
+      <guid>2c474448-c1d6-4afd-bfa6-5510093ff208</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentFolders/contentFolderAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba4dabc9-d83b-401d-8d64-3e4793c337ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentFolders/contentFolderUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7b318547-2a2b-44e2-8267-406d11e3ab6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentItems/contentItemCreate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ab5717c-254f-499e-8d8a-1dbc6d04999c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentItems/contentItemUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c85bc9f5-5809-4de8-8294-14442dafd0e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPlaceholders/contentPlaceholderAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b228d22-6249-49fa-aa41-c4145e04d756</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPlaceholders/contentPlaceholderUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b383367e-7f26-4827-b22e-33e2fbe98a09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPublications/contentPublicationAdd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>497d1fb1-5b03-46b2-8abb-c6d31c902085</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPublications/contentPublicationUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1c06b1c3-53a4-4898-80a5-c5c48b47e235</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPublications/contentPublicationDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d52d3029-b040-42dc-a092-f5ee3e17db7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentItems/contentItemDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba9c4888-6d03-4e5d-87c1-ef2b58d8a217</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentItems/contentItemDeleteBulk</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>83464e7b-5e6c-4454-9389-c68696c5747e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPlaceholders/contentPlaceholderDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8847aed9-d92b-412c-ab80-12b933429735</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentFolders/contentFolderDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7990a1ca-8165-4b39-b0ec-7f1fb1e8d5c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPlaceholders/contentPlaceholderDeleteBulk</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7cc454e5-ada2-4075-a18b-78578202eb61</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleMarketing/DynamicContent/ContentPublications/contentPublicationDeleteBulk</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c84a2899-c811-4abf-823c-be1bade97223</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/ModuleMarketing/PromoAndCoupons/promoCreate</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c9324825-aff7-4135-8185-025bc87a87e6</guid>
+      <guid>17a71ae0-3109-4e57-b8a9-a1100281cbba</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/ModuleMarketing/PromoAndCoupons/promoUpdate_AlternativeWay</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>16ba0547-e7fa-4882-a6b6-b1ad7d323a82</guid>
+      <guid>8b22d566-e6f4-43e9-9102-3cd1a6ab6cec</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/ModuleMarketing/PromoAndCoupons/promoDelete</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>28be7e20-169a-4759-94f8-8dd219c08c59</guid>
+      <guid>33a7e06b-d18d-42d7-a320-7b57a4c6ef10</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/ModuleMarketing/PromoAndCoupons/promoCoupon_createUpdateDelete</testCaseId>
