b2cec721ea454b014917018ba0e5de9da5fca2db
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-07-05 08:48:11 +0300

    consolidate all marketing related suites into one

--- Test Suites/Modules/module_Marketing.ts
+++ Test Suites/Modules/module_Marketing.ts
@@ -14,13 +14,13 @@
    <testCaseLink>
       <guid>d5ebd59f-8f44-4f81-8f02-c38d0fceef2e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/AuthBackToken</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>710c60f2-0035-49df-b492-d7f9d569d395</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/API Coverage/Platform/ApiKeySet</testCaseId>
    </testCaseLink>
    <testCaseLink>
