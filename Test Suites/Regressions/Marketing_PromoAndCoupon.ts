90bee3b52d3f651418c6c8ba8ec9ef18c4aacb36
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-07-05 08:33:55 +0300

    VP-7930: promotion rules test data creation (#285)
    * remove unwanted file
    
    * fix contentPlaceholder script  file issue
    
    * add promo test data creation to regression siute

--- Test Suites/Regressions/Marketing_PromoAndCoupon.ts
+++ Test Suites/Regressions/Marketing_PromoAndCoupon.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Marketing</name>
+   <name>Marketing_PromoAndCoupon</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
