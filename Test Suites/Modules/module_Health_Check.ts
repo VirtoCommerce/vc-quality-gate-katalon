fab83bab4c701336584193bc91d2e3f8addd327a
DariaPonamereva <84708086+DariaPonamereva@users.noreply.github.com>
2022-07-28 17:07:44 +0300

    Health check (#297)
    * Add health check object, test case and test suite
    
    * Update health check object
    
    * Create module HealthCheck
    
    * Delete copies

---
+++ Test Suites/Modules/module_Health_Check.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>module_Health_Check</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7bb44cf7-1429-4f61-832c-ddcf8cb39704</testSuiteGuid>
+   <testCaseLink>
+      <guid>7cb10252-eb60-4f29-b10d-41c73ba8fbe4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Coverage/ModuleHealthCheck/HealthCheck</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
