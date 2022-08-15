2c9591e17b6a0f283f587a3cea59ddc494c4187a
Dasha <daria.ponamareva@virtoway.com>
2022-08-15 18:19:25 +0300

    Update healthcheck: added check for unhealthy status

--- Scripts/API Coverage/ModuleHealthCheck/HealthCheck/Script1658757445141.groovy
+++ Scripts/API Coverage/ModuleHealthCheck/HealthCheck/Script1658757445141.groovy
@@ -3,6 +3,9 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import java.nio.file.WatchService
+
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
@@ -21,13 +24,23 @@ import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 WebUI.comment('TEST CASE: Get information about cache and modules health')
 
 
-'VERIFY THAT STATUS AND DESCRIPTION ARE HEALTHY'
+'GET STATUS AND DESCRIPTION OF MODULES AND CACHE'
 healthyStatus = 'Healthy'
 healthyModulesDecription = 'All modules are loaded'
 healthyCacheDescription = 'Cache is active'
 
+unhealthyStatus = 'Degraded'
+unhealthyModulesDecription = 'Some modules have errors'
+
 healthInformationGet = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.HealthCheck/HealthCheck'))
-WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Description', healthyModulesDecription)
-WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Status', healthyStatus)
-WS.verifyElementPropertyValue(healthInformationGet, '"Cache health".Description', healthyCacheDescription)
-WS.verifyElementPropertyValue(healthInformationGet, '"Cache health".Status', healthyStatus)
+if (WS.getElementPropertyValue(healthInformationGet, '"Modules health".Status')==healthyStatus){
+	WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Status', healthyStatus)
+	WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Description', healthyModulesDecription)
+	WS.verifyElementPropertyValue(healthInformationGet, '"Cache health".Description', healthyCacheDescription)
+	WS.verifyElementPropertyValue(healthInformationGet, '"Cache health".Status', healthyStatus)
+}	else {
+		WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Status', healthyStatus)
+		WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Status', unhealthyStatus)
+		WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Description', unhealthyModulesDecription)
+		println("\nInformation about errors: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Data'))
+}
