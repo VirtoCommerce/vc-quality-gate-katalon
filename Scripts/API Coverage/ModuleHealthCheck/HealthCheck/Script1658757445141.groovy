44ef06b514c77c788bfbdbe4fc2274509a60022a
Evg652 <60473236+Evg652@users.noreply.github.com>
2022-08-19 12:52:54 +0300

    improve how errors are displayed

--- Scripts/API Coverage/ModuleHealthCheck/HealthCheck/Script1658757445141.groovy
+++ Scripts/API Coverage/ModuleHealthCheck/HealthCheck/Script1658757445141.groovy
@@ -14,27 +14,27 @@ WebUI.comment('TEST CASE: Get information about cache and modules health')
 healthyStatus = 'Healthy'
 healthyModulesDecription = 'All modules are loaded'
 healthyCacheDescription = 'Cache is active'
-//unhealthyStatus = 'Degraded'
-//unhealthyModulesDecription = 'Some modules have errors'
 
 
-'GET HEALTH STATUS AND DESCRIPTION OF MODULES AND CACHE'
+'GET AND VERIFY HEALTH DATA'
 healthInformationGet = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.HealthCheck/HealthCheck'))
 if (WS.getElementPropertyValue(healthInformationGet, '"Modules health".Status') == healthyStatus) {
 	WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Description', healthyModulesDecription)
 	WS.verifyElementPropertyValue(healthInformationGet, '"Cache health".Description', healthyCacheDescription)
 	WS.verifyElementPropertyValue(healthInformationGet, '"Cache health".Status', healthyStatus)
 } else {
-		WebUI.comment("\nMODULES HEALTH STATUS IS: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Status'))
-		WebUI.comment("\nMODULES HEAlTH DESCRIPTION IS: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Description'))
+		'DISPLAY ERRORS DATA'
 		responseBody = healthInformationGet.getResponseBodyContent()
 		responseBodyParsed = new JsonSlurper().parseText(responseBody)
-		errorsData = responseBodyParsed."Modules health".Data
+		errorsData = responseBodyParsed."Modules health".Data//['VirtoCommerce.ProfileExperienceApiModule'].Errors
 		errorsDataJson = new groovy.json.JsonBuilder(errorsData).toString()
-		WebUI.comment("\nMODULES ERRORS DATA: \n" +JsonOutput.prettyPrint(errorsDataJson))
-		//WS.verifyElementPropertyValue(healthInformationGet, '"Modules health".Status', healthyStatus)
-		KeywordUtil.markFailed("UNHEALTHY")//just as an alternative way to fail test case
-		//probably we should also cover scenarios for cache failure
-		//need to explore
+		WebUI.comment("\n\n" + "\nMODULES HEALTH STATUS IS: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Status') + "\nMODULES HEAlTH DESCRIPTION IS: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Description') + "\nMODULES ERRORS DATA: \n" +JsonOutput.prettyPrint(errorsDataJson) + "\n\n")
+		KeywordUtil.markFailed("UNHEALTHY")//just as an alternative way to fail a test case
+//		WebUI.comment("\nMODULES HEALTH STATUS IS: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Status'))
+//		WebUI.comment("\nMODULES HEAlTH DESCRIPTION IS: \n" + WS.getElementPropertyValue(healthInformationGet, '"Modules health".Description'))
+//		WebUI.comment("\nMODULES ERRORS DATA: \n" +JsonOutput.prettyPrint(errorsDataJson))
+//      probably we should also cover scenarios for cache failu
+//      need to explore
+		
 }
 
