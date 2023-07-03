5e57d61b43ddf3a97af458a6edff875350d0a3fd
ElenaMutykova <elena.mutykova@virtoway.com>
2023-07-03 17:46:07 +0200

    patchProduct

--- Object Repository/API/backWebServices/VirtoCommerce.Catalog/_ProductPatch.rs
+++ Object Repository/API/backWebServices/VirtoCommerce.Catalog/_ProductPatch.rs
@@ -1,24 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>ProductsGetByIdAndGroup</name>
+   <name>_ProductPatch</name>
    <tag></tag>
-   <elementGuidId>c1a70609-929d-4f78-b844-7439d19a4794</elementGuidId>
+   <elementGuidId>6db386c1-bd80-467e-9233-7112053e0b47</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
-   <autoUpdateContent>true</autoUpdateContent>
+   <autoUpdateContent>false</autoUpdateContent>
    <connectionTimeout>-1</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
-   <httpBodyContent></httpBodyContent>
-   <httpBodyType></httpBodyType>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;Name\&quot;:\&quot;${name}\&quot;,\n  \&quot;IsActive\&quot;:\&quot;${isActive}\&quot;\n\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>Content-Type</name>
       <type>Main</type>
-      <value>text/plain</value>
-      <webElementGuid>a7195e64-2211-4735-a069-17fb7e33d5bd</webElementGuid>
+      <value>application/json</value>
+      <webElementGuid>ca67511d-8a07-4494-a310-a5a6f4638b99</webElementGuid>
    </httpHeaderProperties>
    <httpHeaderProperties>
       <isSelected>true</isSelected>
@@ -26,12 +30,12 @@
       <name>api_key</name>
       <type>Main</type>
       <value>${GlobalVariable.api_key}</value>
-      <webElementGuid>3a9fc034-7697-496d-b116-1d67d76180ef</webElementGuid>
+      <webElementGuid>d8ab1676-b1d7-4575-938c-92111d11fe5a</webElementGuid>
    </httpHeaderProperties>
    <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
-   <restRequestMethod>GET</restRequestMethod>
-   <restUrl>${GlobalVariable.urlBack}/api/catalog/products?ids=${id}</restUrl>
+   <restRequestMethod>PATCH</restRequestMethod>
+   <restUrl>${GlobalVariable.urlBack}/api/catalog/products/${productId}/${language}</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
@@ -42,10 +46,31 @@
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
    <variables>
       <defaultValue>'ec22cd0e-7d28-48b2-b2c9-d3b19cc71e3b'</defaultValue>
-      <description>searched product id</description>
-      <id>8b64082b-4e68-4358-b546-2618a39079d3</id>
+      <description>Id updated product</description>
+      <id>43a1ae75-290c-4d7e-88df-04a0c03bffda</id>
       <masked>false</masked>
-      <name>id</name>
+      <name>productId</name>
+   </variables>
+   <variables>
+      <defaultValue>'en-US'</defaultValue>
+      <description>used language</description>
+      <id>4282c2a0-acca-4379-b526-9949f5e5c9a8</id>
+      <masked>false</masked>
+      <name>language</name>
+   </variables>
+   <variables>
+      <defaultValue>'Katalon_patchProductNew1'</defaultValue>
+      <description>updated product name</description>
+      <id>8e97e33e-d205-49f5-a120-db8867dbd694</id>
+      <masked>false</masked>
+      <name>name</name>
+   </variables>
+   <variables>
+      <defaultValue>'false'</defaultValue>
+      <description>product visibility</description>
+      <id>0db0059c-0082-4dec-bdc2-055f903f4a9b</id>
+      <masked>false</masked>
+      <name>isActive</name>
    </variables>
    <verificationScript>import static org.assertj.core.api.Assertions.*
 
@@ -59,7 +84,7 @@ import com.kms.katalon.core.util.KeywordUtil
 
 ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
 def prettyJson = JsonOutput.prettyPrint(response.getResponseBodyContent())
-//KeywordUtil.logInfo(prettyJson)
+KeywordUtil.logInfo(prettyJson)
 WS.verifyResponseStatusCode(response, 200)
 </verificationScript>
    <wsdlAddress></wsdlAddress>
