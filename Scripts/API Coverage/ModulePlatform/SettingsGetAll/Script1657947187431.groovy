98ea528e2adc0453eb8f22783c35fc83e4640703
AlexTzykin <52533659+AlexTzykin@users.noreply.github.com>
2023-08-28 09:22:56 +0300

    fix verification for settings (#342)

--- Scripts/API Coverage/ModulePlatform/SettingsGetAll/Script1657947187431.groovy
+++ Scripts/API Coverage/ModulePlatform/SettingsGetAll/Script1657947187431.groovy
@@ -20,5 +20,5 @@ import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 WebUI.comment('TEST CASE: Get info about all settings')
 
 response = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.Platform/SettingsGetAll'))
-WS.verifyElementPropertyValue(response, '[0].name', "VirtoCommerce.Platform.Security.AccountTypes")
-WS.verifyElementPropertyValue(response, '[0].defaultValue', "Customer")
+WS.verifyElementPropertyValue(response, '[0].name', "Cart.CronDeleteObsoleteCarts")
+WS.verifyElementPropertyValue(response, '[0].defaultValue', "0 2 */1 * *")
