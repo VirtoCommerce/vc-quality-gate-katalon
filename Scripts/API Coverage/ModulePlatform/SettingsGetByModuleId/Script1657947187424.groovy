da094a350425847c97364232125130a91c6dc5f7
AlexTzykin <52533659+AlexTzykin@users.noreply.github.com>
2023-10-26 18:56:24 +0300

    change moduleId from notification to catalog

--- Scripts/API Coverage/ModulePlatform/SettingsGetByModuleId/Script1657947187424.groovy
+++ Scripts/API Coverage/ModulePlatform/SettingsGetByModuleId/Script1657947187424.groovy
@@ -20,7 +20,7 @@ import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 WebUI.comment('TEST CASE: Get info about all setting within one module')
 
 response = WS.sendRequestAndVerify(findTestObject('API/backWebServices/VirtoCommerce.Platform/SettingsGetByModuleId', [
-	('moduleId') : 'VirtoCommerce.Notifications'
+	('moduleId') : 'VirtoCommerce.Catalog'
 	]))
-WS.verifyElementPropertyValue(response, '[0].moduleId', 'VirtoCommerce.Notifications')
-WS.verifyElementPropertyValue(response, '[0].groupName', 'Notifications|General')
+WS.verifyElementPropertyValue(response, '[0].moduleId', 'VirtoCommerce.Catalog')
+WS.verifyElementPropertyValue(response, '[0].groupName', 'Catalog|General')
