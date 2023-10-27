d3a8a69a120830fc5a63f9f344d1678f80e3f48d
AlexTzykin <52533659+AlexTzykin@users.noreply.github.com>
2023-10-27 16:33:21 +0300

    Disable Core module in platform collection script

--- Test Suites/Modules/Platform_collection.ts
+++ Test Suites/Modules/Platform_collection.ts
@@ -244,7 +244,7 @@
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Modules/module_Core</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
